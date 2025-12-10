832befcc62b035de19a57aecd061e04f4c147838
Alla Yevlampiieva <modan1679@gmail.com>
2025-12-10 16:44:40 +0200

    Task-5: add public endpoint fot login of user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,35 @@
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
 export const registerUser = async (req, res) => {
   res.status(201).json();
 };
 
-export const loginUser = async (req, res) => {
-  res.status(200).json();
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(createHttpError(401, 'Invalid credentials'));
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    return next(createHttpError(401, 'Invalid credentials'));
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
 };
 
 export const logoutUser = async (req, res) => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,10 @@
-// import { celebrate } from 'celebrate';
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
+import { loginUser } from '../controllers/authController.js';
+import { loginUserSchema } from '../validations/authValidation.js';
 
 const router = Router();
+
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
