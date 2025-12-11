c4bbb1a5337c6617fe7f54b040a5e4fe241a58d2
Alla Yevlampiieva <modan1679@gmail.com>
2025-12-12 01:10:50 +0200

    Task-6: add refreshUserSession, authenticate

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  refreshUserSession,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
@@ -15,5 +16,6 @@ const router = Router();
 router.post('/api/auth/register', celebrate(registerSchema), registerUser);
 router.post('/api/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/api/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
