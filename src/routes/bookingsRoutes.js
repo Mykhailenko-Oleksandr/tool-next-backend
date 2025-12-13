7afea707d91f00dbc8984c0872e8b3a68fc892ea
Oleksandr08 <is4uk2011@icloud.com>
2025-12-13 20:15:14 +0200

    task-16: fix new code

--- src/routes/bookingsRoutes.js
+++ src/routes/bookingsRoutes.js
@@ -1,6 +1,20 @@
-import { Router } from 'express';
-// import { celebrate } from 'celebrate';
+import express from 'express';
+import { celebrate } from 'celebrate';
+import {
+  checkAvailability,
+  createBooking,
+} from '../controllers/bookingController.js';
+import { createBookingSchema } from '../validations/bookingValidations.js';
+import { authenticate } from '../middleware/authenticate.js';
+// import { authenticate } from '../middleware/authenticate.js';
 
-const router = Router();
+const router = express.Router();
 
-export default router;
+router.get('/tools/:toolId/availability', checkAvailability);
+
+router.post(
+  '/bookings/:toolId',
+  authenticate,
+  celebrate(createBookingSchema),
+  createBooking,
+);
